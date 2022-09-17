dbf9ffe927b9feb085671c8d3f54b7a80aea31b6
JHDsBR <Jesse2013.jsc@gmail.com>
2022-09-17 19:45:51 -0300

    teste deploy

--- index.js
+++ index.js
@@ -1,38 +1,61 @@
 const { ApolloServer, gql } = require('apollo-server')
 const PORT = process.env.PORT || 5812
 
+
 const typeDefs = gql`
 
     type User {
-        id: ID!
-        name: String
+        id: ID! # id do usuário
+        name: String # nome do usuário
     }
 
+    # get
     type Query {
         users: [User!]! # lista de usuários 
         getUserById(id: ID!): User! # retorna um usuário pelo id
         getUserByName(name: String!): User! # retorna um usuário pelo nome
     }
+
+    # put
+    type Mutation {
+        createUser(name: String!): User!
+    }
 `
 
+// users para teste
 const users = [
-    {id:0, name:"jesse"},
+    {id:0, name:"jessé"},
     {id:1, name:"felipe"},
     {id:2, name:"tom"},
     {id:3, name:"sarah"},
     {id:4, name:"pelé"},
 ]
 
+
 const resolvers = {
+
     Query: {
         users: () => users,
         getUserById: (_, args) => {return users.find((user) => user.id == args.id)},
         getUserByName: (_, args) => {return users.find((user) => user.name == args.name)},
     },
+    
+    Mutation: {
+        createUser: (_, args) => {
+            const newUser = {
+                id: users.at(-1).id+1,
+                name: args.name,
+            }
+            users.push(newUser)
+            return newUser
+        }
+    }
 }
 
+
+// cria o servidor
 const server = new ApolloServer({typeDefs, resolvers})
 
-
+// começa a escutar a porta
 server.listen(PORT).then(({url}) => 
     console.log(`Servidor online -> ${url}`))
--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 import requests
 
-url = 'http://localhost:4000/'
+url = 'http://localhost:5812/'
 
-def GetGraphQL(body:dict) -> requests.Response:
+def GraphQL(body:dict) -> requests.Response:
     return requests.post(url, json=body)
 
-def PostGraphQL(body:dict):
-    pass
+# teste de GET
+def Get():
+    id = "3"
+    
+    body = """
+        query {
+            getUserById(id: "$id") {
+                id
+                name
+            }
+        }
+    """.replace("$id", id)
 
-r = GetGraphQL({"query":"{id}"})
+    r = GraphQL({"query":body})
+    print("Houve um erro")/exit() if r.status_code != 200 else None
+    print(r.json()["data"])
 
-print("Houve um erro")/exit() if r.status_code != 200 else None
 
-print(r.json()["data"])
+# teste de PUT
+def Put():
+    name = "tom"
+
+    body = """
+        mutation {
+            createUser(name: "$name") {
+                id
+                name
+            }
+        }
+    """.replace("$name", name)
+
+    r = GraphQL({"query":body})
+    print("Houve um erro ->", r.text)/exit() if r.status_code != 200 else None
+    print(r.json()["data"])
+
+
+# testes
+Put() # cria
+Get() # pega
